7430a71372f663194f390155b1d1b27523af2547
ye0nuu <dusdn1919@naver.com>
2025-06-18 15:48:24 +0900

    별 찍기 재풀이 : 1~3

---
+++ star_0617/src/Star1_2.java
@@ -0,0 +1,11 @@
+public class Star1_2 {
+    public static void main(String[] args) {
+        // 배열 형태로 생각
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<5; j++) {
+                System.out.printf("[%d %d]", i,j);
+            }
+            System.out.println();
+        }
+    }
+}
